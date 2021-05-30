f81cce22b56bc338e6544af5e93a58900b4d9155
kumarinbox31 <84706801+kumarinbox31@users.noreply.github.com>
2021-05-30 13:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+#     return render_template("home.html")
     return render_template("google634cd1552b118c17.html")
 
 @app.route("/show1")
